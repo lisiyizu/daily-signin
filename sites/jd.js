9b0517bbdd3c77fd79b560f06604f0c6478a004a
ding <playdingnow@gmail.com>
2017-08-02 09:05:12 +0800

    feat(jd): try to use chromeless

--- sites/jd.js
+++ sites/jd.js
@@ -1,30 +1,34 @@
 const config = require('../config');
 
-const Nightmare = require('nightmare');
-
-const nightmare = Nightmare(config.nightmare);
+const { Chromeless } = require('chromeless');
 
 const { urls: URLS, elements: ELES } = config.sites.jd;
 
 const run = () => {
   const { username, password } = config.profile;
+  const chromeless = new Chromeless(config.chromeless);
 
-  return nightmare
+  return chromeless
     .goto(URLS.home)
     .wait(ELES.gotoLogin)
+    .screenshot()
     .click(ELES.gotoLogin)
     .wait(ELES.usernameLoginButton)
     .click(ELES.usernameLoginButton)
-    .wait(ELES.usernameInpu)
+    .wait(ELES.usernameInput)
     .type(ELES.usernameInput, username)
     .type(ELES.passwordInput, password)
-    .click(ELES.loginButton)
-    .wait(ELES.checkinBtn)
-    .click(ELES.checkinBtn)
-    .wait(ELES.checkinSuccess)
-    // "签到成功" if success
-    .evaluate(selector => document.querySelector(selector).innerText, ELES.checkinSuccess)
-    .end();
+    .then((screenshot) => {
+      console.log({ screenshot });
+      return chromeless.end();
+    });
+  //   .click(ELES.loginButton)
+  //   .wait(ELES.checkinBtn)
+  //   .click(ELES.checkinBtn)
+  //   .wait(ELES.checkinSuccess)
+  //   // "签到成功" if success
+  //   .evaluate(selector => document.querySelector(selector).innerText, ELES.checkinSuccess)
+  //   .end();
 };
 
 module.exports = {
